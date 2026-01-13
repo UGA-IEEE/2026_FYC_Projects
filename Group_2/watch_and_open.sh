0b26c42f289bd3248a30e0c420f18eda91efc5db
SN-P946 <snpro405@gmail.com>
2026-01-13 18:26:01 -0500

    updated sh file with enhanced functionality

--- Group_2/watch_and_open.sh
+++ Group_2/watch_and_open.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
-
 WATCH_DIR="/media/ieeegrp2/0403-0201/DCIM/100MEDIA"
 
 echo "Watching folder: $WATCH_DIR"
 
-inotifywait -m -e create "$WATCH_DIR" --format '%f' | while read FILE
+inotifywait -m -e close_write,moved_to --format '%f' "$WATCH_DIR" | while read -r FILE
 do
-    FILEPATH="$WATCH_DIR/$FILE"
-    echo "New file detected: $FILEPATH"
-    code "$FILEPATH"
+  FILEPATH="$WATCH_DIR/$FILE"
+  [[ "$FILE" != *.pdf ]] && continue
+
+  echo "Processing: $FILEPATH"
+  python3 /path/to/pdf_text_extractor.py "$FILEPATH"
 done
