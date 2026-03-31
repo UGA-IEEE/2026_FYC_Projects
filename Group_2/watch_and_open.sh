1b89f875d65f17e87ed3773720f61e5e73516469
SN-P946 <snpro405@gmail.com>
2026-03-31 17:22:44 -0400

    Updated bash file with improvements
    Change inotifywait event from 'create' to 'close_write' and update file handling to execute a Python script when a new file is ready.

--- Group_2/watch_and_open.sh
+++ Group_2/watch_and_open.sh
@@ -4,9 +4,11 @@ WATCH_DIR="/media/ieeegrp2/0403-0201/DCIM/100MEDIA"
 
 echo "Watching folder: $WATCH_DIR"
 
-inotifywait -m -e create "$WATCH_DIR" --format '%f' | while read FILE
+inotifywait -m -e close_write "$WATCH_DIR" --format '%f' | while read FILE
 do
     FILEPATH="$WATCH_DIR/$FILE"
-    echo "New file detected: $FILEPATH"
-    code "$FILEPATH"
+    if [ -f "$FILEPATH" ]; then
+        echo "New file ready: $FILEPATH"
+        python3 /full/path/to/PDF_Text.py "$FILEPATH"
+    fi
 done
